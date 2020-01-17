b8cb65d8ea7c25dfb406f14d849a65b4b12d7713
Shotaro Tao <taoshotaro@gmail.com>
2020-01-17 18:21:54 +0900

    Added support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Mag",
+    platforms: [
+        .iOS(.v10),
+        .tvOS(.v10),
+        .macOS(.v10_12)
+    ],
+    products: [
+        .library(
+            name: "Mag",
+            targets: ["Mag"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Mag",
+            path: "Mag"
+        ),
+        .testTarget(
+            name: "MagTests",
+            dependencies: ["Mag"],
+            path: "MagTests"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
